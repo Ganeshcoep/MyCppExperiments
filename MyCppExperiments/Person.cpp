5a2294ef7703ece8262fded8109409ae9ff43f2d
ganeshcoep <ganeshsalvi.coep@gmail.com>
2018-07-18 23:48:05 -0500

    Add overloaded operators and friend function
    Added the mmeber functions to load the operator <
    Addded free function to load the operator <
    Added the friend function to load the operator <

--- MyCppExperiments/Person.cpp
+++ MyCppExperiments/Person.cpp
@@ -7,7 +7,7 @@ using std::cout;
 using std::endl;
 //using std::string; //defined in the header file so no need here?
 
-Person::Person(const string & fname, const string& lname):firstName(fname), lastName(lname)
+Person::Person(const string & fname, const string& lname, const int& ht):firstName(fname), lastName(lname), height(ht)
 {
 }
 
@@ -16,7 +16,28 @@ Person::~Person()
 	cout << "Desctructor got called" << endl;;
 }
 
-string Person::getFirstName()
+string Person::getFullName() const
 {
-	return firstName;
+	return getFirstName() + " " + getLastName();
+}
+
+bool Person::operator<(const Person & p)
+{
+	return height < p.getHeight();
+}
+
+bool Person::operator<(const int & ht)
+{
+	return height < ht;
+}
+
+bool operator<(const float&f, const Person& p)
+{
+	cout << "float operator < overloaded got called" << endl;
+	return f < p.height;
+}
+
+bool operator<(const int & ht, const Person & p)
+{
+	return ht < p.getHeight();
 }
--- MyCppExperiments/Person.h
+++ MyCppExperiments/Person.h
@@ -10,11 +10,20 @@ private:
 	string lastName;
 	int height;
 
+	friend bool operator<(const float&f, const Person& p);
+
 public:
 	Person() = default;
-	Person(const string & fname, const string& lname);
+	Person(const string & fname, const string& lname, const int& ht);
 	virtual ~Person();
 
-	string getFirstName();
+	inline string getFirstName() const { return firstName; };
+	inline string getLastName() const { return lastName; };	
+	inline int getHeight() const { return height; };
+	string getFullName() const;
+
+	bool operator<(const Person& p);
+	bool operator<(const int& hti);
 };
 
+bool operator<(const int& ht, const Person& p);
